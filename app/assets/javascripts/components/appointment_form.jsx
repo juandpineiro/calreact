263e561dd5351360f99c168b36c9da91a360202c
Juan Piñeiro <juandpineiro@gmail.com>
2018-04-06 20:50:59 +0000

    Handle user input (pending)

--- app/assets/javascripts/components/appointment_form.jsx
+++ app/assets/javascripts/components/appointment_form.jsx
@@ -1,11 +1,20 @@
 var AppointmentForm = React.createClass({
+  handleChange: function() {
+    var name = e.target.name;
+    obj = {};
+    obj[name] = e.target.value;
+    /* continue here */
+  },
+
   render: function() {
     return (
       <div>
         <h2>Make a new appointment</h2>
         <form>
-          <input name='title' placeholder='Appointment title' />
-          <input name='appt_time' placeholder='Date and time' />
+          <input name='title' placeholder='Appointment title' 
+                 value={this.props.input_title}
+                 onChange={this.handleChange} />
+          <input name='appt_time' placeholder='Date and time' value={this.props.input_appt_time} />
           <input type='submit' value='Meke Appointment' />
         </form>
       </div>
