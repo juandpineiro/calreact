9d2652d2788095b935bceed8822768c238e44b2a
Juan Piñeiro <juandpineiro@gmail.com>
2018-04-09 19:47:31 -0400

    Update Appointment component to ES6

--- app/assets/javascripts/components/appointment.es6.jsx
+++ app/assets/javascripts/components/appointment.es6.jsx
@@ -1,10 +1,10 @@
-var Appointment = React.createClass({
-  render: function() {
-    return(
+class Appointment extends React.Component {
+  render () {
+    return (
       <div className='appointment'>
         <h3>{this.props.appointment.title}</h3>
         <p>{formatDate(this.props.appointment.appt_time)}</p>
       </div>
     )
   }
-});
+}
